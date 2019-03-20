626e94043a3b7f485a2feed61b164049b8759b76
emaramar <48704541+emaramar@users.noreply.github.com>
2019-03-20 16:06:16 +0800

    test function changes

--- ProjectViemed/POMProject/Test/login.py
+++ ProjectViemed/POMProject/Test/login.py
@@ -13,24 +13,22 @@ class LoginTest(unittest.TestCase):
         cls.driver.implicitly_wait(30)
         cls.driver.maximize_window()
 
-    def test01(self):
+    def test_login_valid(self):
         driver = self.driver
         login = LoginPage(driver)
         driver.get("https://telemed.viemed.io")
         login_text = driver.find_element_by_xpath("//p[@class='h4 text-center']").text
-        # driver.save_screenshot('C:/Users/emaramar/PycharmProjects/Edjel/reports/loginpage.png')
         self.assertEqual("Login",login_text,"The Login page is not displayed.")
         print("Login Page is displayed.")
         login.input_username("viemed.automation.tester@gmail.com")
         login.input_password("50BF57F3-523c-4082-9870-04eb33deab9e")
         login.click_login_button()
-        # driver.save_screenshot('C:/Users/emaramar/PycharmProjects/Edjel/reports/homepage.png')
         return_txt = self.driver.find_element_by_xpath("//h2[contains(text(),'PROVIDER HOME')]").text
         self.assertEqual("PROVIDER HOME",return_txt, "The Home page is not displayed.")
         print("Login is successful.")
         time.sleep(10)
 
-    def test02(self):
+    def test_user_status(self):
         driver = self.driver
         home = Homepage(driver)
         home.btn_click()
